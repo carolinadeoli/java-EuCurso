c7c67b6485a68fce5f508b997a059be831832b6a
carolinadeoli <121107004+carolinadeoli@users.noreply.github.com>
2023-09-22 22:30:21 +0000

    codigojavaEuCurso

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-// class Main {
-//   public static void main(String[] args) {
-//     System.out.println("Hello world!");
-//   }
-// }
 
 public class Main{
   public static void main(String[] args){
