77142d93bc9a6d09eed3380a46a1fea742429e5c
carolinadeoli <121107004+carolinadeoli@users.noreply.github.com>
2023-09-23 21:03:25 +0000

    organizando arquivos

---
+++ tipos.java
@@ -0,0 +1,26 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("Hello world!");
+
+    //tipos primitivos
+    int idade = 26;
+    idade +=10;
+    // declara variável
+    // idade = 27; altera valor da variavel acima
+    double altura = 1.67;
+    boolean casado = false;
+    
+    System.out.println(idade);
+    System.out.println(altura);
+    System.out.println(casado)
+    
+      //tipos wrappers
+
+    // Integer idadeW= 26;
+    // Double  alturaW= 1.67;
+    // Boolean casada = false;
+
+    ;
+    
+  }
+}
