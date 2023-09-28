89a17ef682639995da5d5775426a5542c44ac344
carolinadeoli <121107004+carolinadeoli@users.noreply.github.com>
2023-09-28 22:58:20 +0000

    exercicios

---
+++ exdowhile.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args){
+  int contagem = 1;
+    do{
+      System.out.println(contagem);
+      contagem++;
+    }while(contagem<6);
+    System.out.println("fim da contagem");
+  }
+}
