983a06be9956de3a7d54800d2c97ca3749ff9b6f
carolinadeoli <121107004+carolinadeoli@users.noreply.github.com>
2023-09-28 22:34:26 +0000

    desafios

---
+++ desafioifelse.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String[] args){
+  int num =0;
+    if(num >0){
+      System.out.println("O número é positivo");    
+    } else if(num <0){
+      System.out.println("O número é negativo.");
+    } else{
+      System.out.println("O número é zero.");
+    }
+   
+  }
+}
---
+++ switchcaseex.java
@@ -0,0 +1,19 @@
+public class Main{
+  public static void main(String[] args){
+  int opcao =2;
+    switch(opcao){
+      case 1:
+        System.out.println("Opção 1");
+        break;
+      case 2:
+        System.out.println("Opção 2");
+        break;
+      case 3:
+        System.out.println("Opção 3");
+        break;
+      default:
+        System.out.println("Opção inválida");
+    }
+   
+  }
+}
